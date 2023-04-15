f419f7a2b87238978c5dfb1ed40645c3c69317a2
Yash Singh <yash9vardhan@gmail.com>
2023-04-15 14:52:51 +0530

    meta connect functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,21 @@ import { About } from './pages/About'
 import { Explore } from './pages/Explore'
 import { Contact } from './pages/Contact'
 import { Help } from './pages/Help'
+import VoteContext from './context/voteContext/VoteContext';
+
 const App = () => {
   return (
-    <div className="bg-[#6C63FF] overflow-hidden w-full" style={{ height: "100vh" }}>
-      <Link to="/" className='absolute text-[#504e68] z-10 font-semibold top-14 left-14 text-2xl'>E-VOTING</Link>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/explore' element={<Explore />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/help' element={<Help />} />
-      </Routes>
-    </div>
-
+      <div className="bg-[#6C63FF] overflow-hidden w-full" style={{ height: "100vh" }}>
+        <Link to="/" className='absolute text-[#504e68] z-10 font-semibold top-14 left-14 text-2xl'>E-VOTING</Link>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/explore' element={<Explore />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/help' element={<Help />} />
+        </Routes>
+      </div>
   )
 }
 
--- client/src/context/voteContext/VoteState.jsx
+++ client/src/context/voteContext/VoteState.jsx
@@ -1,8 +1,58 @@
 import VoteContext from "./VoteContext";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const getContractInfo = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+  // MetaMask requires requesting permission to connect users accounts
+  // await provider.send("eth_requestAccounts", []);
+
+  // The MetaMask plugin also allows signing transactions to
+  // send ether and pay to change state within the blockchain.
+  // For this, you need the account signer...
+  const signer = provider.getSigner();
+  // const transactionContract = new ether.Contract()   //Address, //ABI, signer)
+
+  console.log({
+    signer,
+    provider
+  })
+}
+
 
 export const VoteState = (props) => {
+  const Navigate = useNavigate();
+
+  const [connectedAccount, setCurrentAccount] = useState();
+
+  const checkIfWalletIsConnected = async() => {
+    if(!ethereum) return alert("Please Install MetaMask");
+
+    const accounts = await ethereum.request({method: 'eth_accounts'});
+
+    console.log(accounts);
+  }
+
+  const connectWallet = async() => {
+    try{
+      if(!ethereum) return alert("Please Install MetaMask");
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+      setCurrentAccount(accounts[0]);
+      Navigate("/explore");
+    } catch (error){
+      console.log(error);
+    }
+  }
+
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  },[]);
+
   return (
-    <VoteContext.Provider value={{}}>
+    <VoteContext.Provider value={{connectWallet}}>
         {props.children}
     </VoteContext.Provider>
   )
